2cc3592ee6a6c1d49e3ef1878394eb33b49430c0
Gus Crawford <crawford.gus@gmail.com>
2021-04-24 21:22:11 -0400

    stdin can be more easily mocked

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
-console.info('Your app starts...');
-gulpStandardInput().then(
-    (input)=>console.log(`Your app runs:\n${input}`)
-).catch(
-    (e)=>console.error(e)
-);
-function gulpStandardInput() {
+export function gulpStandardInput(stdin?:NodeJS.ReadStream) {
     return new Promise(
         (resolve, reject)=>{
             try {
                 let streamed = '';
-                process.stdin.on('data', (chunk) => streamed += `${chunk}`);
-                process.stdin.on('end', ()=> resolve(streamed));
-                process.stdin.on('error', (e)=>reject(e));
+                stdin?.on('data', (chunk) => streamed += `${chunk}`);
+                stdin?.on('end', ()=> resolve(streamed));
+                stdin?.on('error', (e)=>reject(e));
             } catch(e) {
                 reject(e);
             }
